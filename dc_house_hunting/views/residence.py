819f93442a950d933ad68061c991856d785c7bb2
John Haiducek <jhaiduce@gmail.com>
2020-09-14 20:49:08 -0400

    Added pre- and post-processing of Residence form serialization

--- dc_house_hunting/views/residence.py
+++ dc_house_hunting/views/residence.py
@@ -1,8 +1,50 @@
+from pyramid.events import subscriber
 from .crud import CRUDView, ViewDbInsertEvent,ViewDbUpdateEvent
 from ..models import Residence
 from colanderalchemy import SQLAlchemySchemaNode
 import colander
 import deform
+
+@colander.deferred
+def get_parkingtype_widget(node,kw):
+    from ..models import ParkingType
+    dbsession=kw['request'].dbsession
+    parkingtypes=dbsession.query(ParkingType)
+    choices=[('','')]+[(parkingtype.id, parkingtype.name) for parkingtype in parkingtypes]
+    return deform.widget.SelectWidget(values=choices)
+
+@colander.deferred
+def get_residencetype_widget(node,kw):
+    from ..models import ResidenceType
+    dbsession=kw['request'].dbsession
+    residencetypes=dbsession.query(ResidenceType)
+    choices=[('','')]+[(residencetype.id, residencetype.name) for residencetype in residencetypes]
+    return deform.widget.SelectWidget(values=choices)
+
+@subscriber(ViewDbInsertEvent, ViewDbUpdateEvent)
+def finalize_residence_fields(event):
+
+    from ..models import Location
+
+    """
+    Post-process an automatically deserialized Residence object
+    """
+
+    if isinstance(event.obj,Residence):
+
+        # Update the location field
+        if event.obj.location is None:
+            event.obj.location=Location()
+
+        event.obj.location.street_address=event.appstruct['address']
+        event.obj.location.city=event.appstruct['city']
+        if event.appstruct['state']:
+            event.obj.location.state=event.appstruct['state']
+        if event.appstruct['zip']:
+            event.obj.location.postal_code=event.appstruct['zip']
+
+        event.obj.parkingtype_id=event.appstruct['parkingtype']
+        event.obj.residencetype_id=event.appstruct['residencetype']
 
 class ResidenceCRUD(CRUDView):
 
@@ -14,10 +56,21 @@ class ResidenceCRUD(CRUDView):
                 colander.String(),
                 name='address'),
             colander.SchemaNode(
+                colander.String(),
+                name='city'),
+            colander.SchemaNode(
+                colander.String(),
+                name='state'),
+            colander.SchemaNode(
+                colander.String(),
+                name='zip',
+                missing=None),
+            colander.SchemaNode(
                 colander.Integer(),
                 name='residencetype',
                 title='Residence type',
-                widget=deform.widget.SelectWidget()
+                widget=get_residencetype_widget,
+                missing=None
             ),
             'bedrooms',
             'bathrooms',
@@ -29,7 +82,8 @@ class ResidenceCRUD(CRUDView):
                 colander.Integer(),
                 name='parkingtype',
                 title='Parking',
-                widget=deform.widget.SelectWidget()
+                widget=get_parkingtype_widget,
+                missing=None
             ),
             'notes',            
         ],
@@ -44,5 +98,23 @@ class ResidenceCRUD(CRUDView):
             }
         }
     )
+
+    def dictify(self,obj):
+        """
+        Serialize a Residence object to a dict for CRUD view
+        """
+
+        appstruct=super(ResidenceCRUD,self).dictify(obj)
+
+        if obj.location is not None:
+            appstruct['address']=obj.location.street_address
+            appstruct['city']=obj.location.city
+            appstruct['state']=obj.location.state
+            appstruct['zip']=obj.location.postal_code
+
+        appstruct['residencetype']=obj.residencetype_id
+        appstruct['parkingtype']=obj.parkingtype_id
+
+        return appstruct
 
     url_path='/residence'
