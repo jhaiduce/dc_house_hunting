67f2220ced20cd7eb996ad388bf2ef3e3e3a8050
John Haiducek <jhaiduce@gmail.com>
2020-09-17 09:34:15 -0400

    Add header to default view

--- dc_house_hunting/views/default.py
+++ dc_house_hunting/views/default.py
@@ -2,10 +2,16 @@ from pyramid.view import view_config
 from pyramid.response import Response
 
 from sqlalchemy.exc import DBAPIError
+from .header import view_with_header
 
 from .. import models
 
+class default_views(object):
 
-@view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
-def my_view(request):
-    return {'project': 'DC House Hunting'}
+    def __init__(self,request):
+        self.request=request
+
+    @view_with_header
+    @view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
+    def my_view(request):
+        return {'project': 'DC House Hunting'}
