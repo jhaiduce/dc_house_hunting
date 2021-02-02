7d939fe0fb6ede920f2a613ceb4511dde8213362
John Haiducek <jhaiduce@gmail.com>
2021-02-01 22:53:32 -0500

    Set selinux labels before running integration tests.

--- test.sh
+++ test.sh
@@ -13,6 +13,13 @@ $VENV/bin/pytest -q dc_house_hunting/migration_tests.py
 # Generate integration test secrets
 python generate_secrets.py --ini-template integration_test.ini.tpl --ini-filename integration_test.ini --secretsdir integration_test_secrets
 
+secret_files=("integration_test_secrets/integration_test.ini" "integration_test_secrets/production.ini" "integration_test_secrets/db_root_pw" "integration_test_secrets/rabbitmq_password" "integration_test_secrets/server-cert.pem" "integration_test_secrets/server-key.pem" "integration_test_secrets/ca.pem" "integration_test_secrets/storage_key.keyfile" "integration_test_secrets/localhost.localdomain.key" "integration_test_secrets/localhost.localdomain.crt" "integration_test_secrets/dhparams.pem" "nginx/common.conf" "nginx/common_location.conf" "nginx/default.conf" "nginx/househunting.conf" "nginx/ssl.conf" "mysql-config-househunting.cnf" "secrets/production.ini" "secrets/dhparams.pem")
+
+for file in "${secret_files[@]}"
+do
+    chcon -t svirt_sandbox_file_t "$file"
+done
+
 # Build images
 sudo docker-compose -f docker-compose.yml -f docker-compose.test.yml -f docker-compose.migrate.yml -f docker-compose.integration_tests.yml build
 
